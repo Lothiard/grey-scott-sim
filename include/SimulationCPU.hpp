6425a94b917371a229aac6d04449efc0dd7dbd78
Restye János Barnabás <barna0801@gmail.com>
2025-12-07 09:34:20 +0100

    feat: add CPU serial implementation

---
+++ include/SimulationCPU.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <vector>
+
+namespace GreyScott {
+    struct SimulationParams;
+
+    class SimulationCPU {
+    public:
+        SimulationCPU(int width, int height);
+        ~SimulationCPU() = default;
+
+        SimulationCPU(const SimulationCPU&) = delete;
+        SimulationCPU& operator=(const SimulationCPU&) = delete;
+
+        void initialize();
+        void step(const SimulationParams& params);
+        void reset();
+
+        const float* getData() const { return m_data.data(); }
+        int getWidth() const { return m_width; }
+        int getHeight() const { return m_height; }
+
+    private:
+        void initializeState();
+        float computeLaplacian(const std::vector<float>& field, int x, int y, int component);
+
+        int m_width;
+        int m_height;
+        std::vector<float> m_data;
+        std::vector<float> m_dataNext;
+    };
+
+} // namespace GreyScott
